0c761ccce3ea7f1cb12f53e24c4d2e72355c1688
phaiden <paiden@outlook.com>
2015-04-15 16:05:35 +0200

    Add missing file to VC

---
+++ Source/Nett/TomlComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nett
+{
+    internal class TomlComment
+    {
+        public string CommentText { get; private set; }
+        public CommentLocation Location { get; private set; }
+
+        public TomlComment(string commentText, CommentLocation location)
+        {
+            this.CommentText = commentText;
+            this.Location = location;
+        }
+    }
+}
