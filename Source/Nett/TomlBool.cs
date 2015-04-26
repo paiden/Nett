465e89e209bbd4c353a01ee03ebe19b7840a2939
phaiden <paiden@outlook.com>
2015-04-26 15:28:14 +0200

    Add missing file

---
+++ Source/Nett/TomlBool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nett
+{
+    public sealed class TomlBool : TomlValue<bool>
+    {
+        public TomlBool(bool value)
+            : base(value)
+        {
+
+        }
+    }
+}
