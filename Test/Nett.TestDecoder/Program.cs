107a859efada04d52c09e425523e7ace659c4cba
paiden <paiden@outlook.com>
2015-11-28 15:06:19 +0100

    Add Test Decoder

---
+++ Test/Nett.TestDecoder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Nett.TestDecoder
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            TextReader reader = Console.In;
+            TextWriter writer = Console.Out;
+            string line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = reader.ReadLine()) != null)
+            {
+                sb.Append(line);
+            }
+
+            try
+            {
+                var t = Toml.ReadString(sb.ToString());
+                var dict = t.ToDictionary();
+                var json = JsonConvert.SerializeObject(dict);
+                writer.Write(json);
+                return 0;
+            }
+            catch (Exception)
+            {
+                return 1;
+            }
+        }
+    }
+}
---
+++ Test/Nett.UnitTests/ReadInvalidTomlTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Nett.UnitTests
+{
+    public class ReadInvalidTomlTests
+    {
+        [Fact(DisplayName = "Reading mixed arrays should cause parse exception")]
+        public void ReadToml_WhenArrayTypesMixed_ThrowsExc()
+        {
+            // Act
+            Action a = () => Toml.ReadString(@"arrays-and-ints =  [1, [""Arrays are not integers.""]]");
+
+            // Assert
+            a.ShouldThrow<Exception>();
+        }
+    }
+}
