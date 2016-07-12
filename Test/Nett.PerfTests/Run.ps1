1eb9afa73b5524da684884e9df124c273a49453e
paiden <paiden@outlook.com>
2016-07-12 19:26:09 +0200

    Add perf test stuff, rebase this later

---
+++ Test/Nett.PerfTests/Run.ps1
@@ -0,0 +1,8 @@
+﻿$configuration = 'Release'
+$pkgbase = Join-Path $PSScriptRoot ..\..\Solutions\Nett\packages
+$prjDir = $PSScriptRoot
+$runner = (Get-ChildItem "$pkgbase\*\NBench.Runner.exe" -Recurse).FullName
+$asm = Join-Path $prjDir "bin\$configuration\Nett.PerfTests.dll"
+$perfres = Join-Path $prjDir PerfTestResults
+
+& $runner $asm output-directory=$perfres
