53c17dbb36b4ca2b014bd959309bcd9ee5f7a834
paiden <paiden@outlook.com>
2016-08-14 13:08:40 +0200

    Set assembly information for 0.5.0 release

--- Source/Nett/Properties/AssemblyInfo.cs
+++ Source/Nett/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Nett")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,8 +26,9 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5")]
+[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyInformationalVersion("0.5.0")]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("Nett.UnitTests.Internal")]
 [assembly: InternalsVisibleTo("Nett.PerfTests")]
