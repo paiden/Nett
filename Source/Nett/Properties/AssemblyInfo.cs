07e23287aff9bdea007dfa555afe867c630eac0b
paiden <paiden@outlook.com>
2017-06-19 13:11:29 +0200

    Update Assembly version numbers

--- Source/Nett/Properties/AssemblyInfo.cs
+++ Source/Nett/Properties/AssemblyInfo.cs
@@ -26,9 +26,9 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6")]
-[assembly: AssemblyFileVersion("0.6.2")]
-[assembly: AssemblyInformationalVersion("0.6.2")]
+[assembly: AssemblyVersion("0.7")]
+[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
 [assembly: CLSCompliant(true)]
 
 [assembly: InternalsVisibleTo("Nett.Tests.Internal")]
