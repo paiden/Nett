03fef3b383b3789696cc6ab685a32f91c40676e6
paiden <paiden@outlook.com>
2017-08-27 11:12:21 +0200

    Nett: Add comment handling API documentation

--- Source/Nett/TomlTable.Operations.cs
+++ Source/Nett/TomlTable.Operations.cs
@@ -7,37 +7,99 @@ namespace Nett
 {
     using CommentsOp = Func<InputComments, IEnumerable<TomlComment>>;
 
+    /// <summary>
+    /// Interface representing a built TOML table combine operation.
+    /// </summary>
+    /// <remarks>
+    /// Use <see cref="TomlTable.Combine(Func{ITargetSelector, ITableCombiner})"/> to create and consume combine operations.
+    /// </remarks>
     public interface ITableCombiner
     {
     }
 
+    /// <summary>
+    /// Builder interface to select the target table for a table combine operation.
+    /// </summary>
     public interface ITargetSelector
     {
+        /// <summary>
+        /// Selects the target table of a table combine operation.
+        /// </summary>
+        /// <param name="target">The target table. Will not get modified.</param>
+        /// <returns>A builder object allowing to select the source table for the combine operation.</returns>
+        /// <exception cref="ArgumentNullException">If target is <b>null</b>.</exception>
         ISourceSelector Overwrite(TomlTable target);
     }
 
+    /// <summary>
+    /// Builder interface to select the source table for a table combine operation.
+    /// </summary>
     public interface ISourceSelector
     {
+        /// <summary>
+        /// Selects the source table for a table combine operation.
+        /// </summary>
+        /// <param name="source">The source table.</param>
+        /// <returns>A builder object allowing to select the comment operation or set the rows for the operation.</returns>
         ICommentOperationOrRowSelector With(TomlTable source);
     }
 
+    /// <summary>
+    /// Builder interface to select the rows the combine operation will process.
+    /// </summary>
     public interface IRowSelector
     {
+        /// <summary>
+        /// All rows existing in source will be processed. New rows will be added to the result table. Rows already existing in
+        /// the target table will be replaced.
+        /// </summary>
+        /// <returns>The built combine operation.</returns>
         ITableCombiner ForAllSourceRows();
 
+        /// <summary>
+        /// All rows in the target table will be overwritten by the equivalent rows from the source table.
+        /// </summary>
+        /// <returns>The built combine operation.</returns>
         ITableCombiner ForAllTargetRows();
 
+        /// <summary>
+        /// Adds all existing rows from the target table and rows not existing yet in the target table to the resulting table.
+        /// Effectively this is an add new rows operation.
+        /// </summary>
+        /// <returns>The built combine operation.</returns>
         ITableCombiner ForRowsOnlyInSource();
     }
 
+    /// <summary>
+    /// Builder interface to select how comments should be handled or what rows should get processed by the combine operation.
+    /// </summary>
     public interface ICommentOperationOrRowSelector : IRowSelector
     {
+        /// <summary>
+        /// Replace or append comments in the target table with comments from the source table for rows that are processed.
+        /// </summary>
+        /// <param name="append">If <b>true</b> appends comments instead of replacing the target table comments.</param>
+        /// <returns>A builder object that allows to select the rows to process.</returns>
         IRowSelector IncludingAllComments(bool append = false);
 
+        /// <summary>
+        /// Copies comments from the source table if the target table row does not have any comments yet.
+        /// </summary>
+        /// <returns>A builder object that allows to select the rows to process.</returns>
         IRowSelector IncludingNewComments();
 
+        /// <summary>
+        /// Keep comments from the target table and do not copy comments from the source table to the resulting table.
+        /// </summary>
+        /// <returns>A builder object that allows to select the rows to process.</returns>
         IRowSelector ExcludingComments();
 
+        /// <summary>
+        /// Specify custom func to control how comments should be processed.
+        /// </summary>
+        /// <param name="combiner">A func to control how comments should be handled.</param>
+        /// <returns>A builder object that allows to select the rows to process.</returns>
+        /// <exception cref="ArgumentNullException">If <i>combiner</i> is <b>null</b>.</exception>
         IRowSelector IncludingComments(CommentsOp combiner);
     }
 
@@ -51,6 +113,13 @@ namespace Nett
         TomlTable Execute();
     }
 
+    /// <summary>
+    /// Struct representing the input data for a custom comment combiner operation.
+    /// </summary>
+    /// <remarks>
+    /// Used as the input argument for a custom comment handler func for
+    /// <see cref="ICommentOperationOrRowSelector.IncludingComments(CommentsOp)"/>
+    /// </remarks>
     public struct InputComments
     {
         internal InputComments(IEnumerable<TomlComment> tgt, IEnumerable<TomlComment> src)
@@ -59,8 +128,16 @@ namespace Nett
             this.SourceComments = src;
         }
 
+        /// <summary>
+        /// Gets the comments of the target table for the row being processed.
+        /// Will be an empty collection for rows that do not exist in the target table.
+        /// </summary>
         public IEnumerable<TomlComment> TargetComments { get; internal set; }
 
+        /// <summary>
+        /// Gets the comment of the source table for the row being processed.
+        /// Will be and empty collection for rows that do not exist in the source table.
+        /// </summary>
         public IEnumerable<TomlComment> SourceComments { get; internal set; }
     }
 
