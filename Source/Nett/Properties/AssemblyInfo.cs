f5c45d15ca419ca3c424d79dd8c55a8a61885af2
paiden <paiden@outlook.com>
2017-09-29 12:03:48 +0200

    Fix code anlyzer issues

--- Source/Nett/Properties/AssemblyInfo.cs
+++ Source/Nett/Properties/AssemblyInfo.cs
@@ -2,24 +2,8 @@
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: CLSCompliant(true)]
-
 [assembly: InternalsVisibleTo("Nett.Tests.Internal")]
 [assembly: InternalsVisibleTo("Nett.Coma.Tests.Internal")]
 [assembly: InternalsVisibleTo("Nett.Coma")]
