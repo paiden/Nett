0ee05131078395fa452c94c15603b6ff96eedef1
paiden <paiden@outlook.com>
2018-08-17 17:40:02 +0200

    Tests: Init commit for new perf benchmarking suite
    Once again switching PerfTest framework. NBench tests not a single time worked out of
    the box when not run for quite some while. A lot of new good stuff and better Doc
    in PerfDotNet now. Hopefully this is the last switch.

---
+++ Test/Nett.Tests.Performance/Benchmarks/ReadTomlStringBenchmark.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Newtonsoft.Json;
+
+namespace Nett.Tests.Performance.Benchmarks
+{
+    [ShortRunJob()]
+    [MarkdownExporter]
+    public class ReadTomlStringBenchmark
+    {
+        private string InputToml;
+        private string InputJson;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var input = new InputData();
+
+            this.InputToml = Toml.WriteString(input);
+            this.InputJson = JsonConvert.SerializeObject(input);
+        }
+
+        [Benchmark(Baseline = true)]
+        public object ReadJson()
+        {
+            return JsonConvert.DeserializeObject(this.InputJson);
+        }
+
+        [Benchmark]
+        public object ReadToml()
+        {
+            return Toml.ReadString(this.InputToml);
+        }
+
+
+        public class InputData
+        {
+            public long A { get; set; } = 1;
+
+            public double B { get; set; } = 2;
+
+            public List<long> C { get; set; } = new List<long>() { 1, 2, 3 };
+
+            public string D { get; set; } = "Hello this is a string";
+
+            public Inner I { get; set; } = new Inner();
+
+            public List<Inner> ThisIsAListOfInner { get; set; } = new List<Inner>() { new Inner(), new Inner(), new Inner() };
+
+            public List<Name> Names { get; set; } = new List<Name>()
+            {
+                new Name("Peter", "Luxer"),
+                new Name("Sergio", "Combucha"),
+                new Name("Salasar", "Smith"),
+            };
+        }
+
+        [TreatAsInlineTable]
+        public class Inner
+        {
+            public int[][] A { get; set; } = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4 } };
+        }
+
+        public class Name
+        {
+            public string ForeName { get; set; }
+
+            public string SurName { get; set; }
+
+            public Name(string f, string s)
+            {
+                this.ForeName = f;
+                this.SurName = s;
+            }
+        }
+    }
+}
---
+++ Test/Nett.Tests.Performance/Program.cs
@@ -0,0 +1,22 @@
+﻿
+#pragma warning disable S125 // Sections of code should not be "commented out"
+
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Nett.Tests.Performance.Benchmarks;
+
+namespace Nett.Tests.Performance
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cfg = ManualConfig.Create(DefaultConfig.Instance)
+            //    .With(ExecutionValidator.DontFailOnError);
+            ;
+
+            BenchmarkRunner.Run<ReadTomlStringBenchmark>(cfg);
+        }
+    }
+}
